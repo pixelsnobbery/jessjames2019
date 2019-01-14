2b8081dc2f301eafed07c280d7ae5e1a4e27c869
James <james@pixelsnobbery.com>
2019-01-13 18:50:07 -0800

    fixed success page typo

--- src/pages/success.js
+++ src/pages/success.js
@@ -6,8 +6,8 @@ import Theme from '../config/theme'
 import { ThemeProvider } from 'styled-components'
 
 import Section from '../components/layouts/section'
-]
-import SectionTitle from '../components/layouts/section-title';
+
+import SectionTitle from '../components/layouts/section-title'
 
 
 const Success = () => (
@@ -17,9 +17,6 @@ const Success = () => (
       <Section>
         <SectionTitle text="Success!"></SectionTitle>
       </Section>
-
-
-
 
     </Layout>
   </ThemeProvider>
